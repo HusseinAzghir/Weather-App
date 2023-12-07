5be3e4d42622a0da8f894ac759844672887bbb3a
HusseinAzghir <husain.azghir@gmail.com>
2023-12-07 12:46:00 +0100

    WeatherApp

---
+++ main.py
@@ -0,0 +1,139 @@
+import json
+import sys
+import urllib.request
+
+import requests
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+from style import StyleWindow
+import threading
+
+
+class Main:
+    def __init__(self, root):
+        self.root = root
+        self.class_style = StyleWindow(self.root)
+
+        self.URL = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
+        self.API = '06051f3cad0ad7ba821ba795bf6d124f'
+        self.list_country = list()
+        self.check = ""
+        self.is_guide_shown = True
+
+        self.add_search_field()
+        self.add_search_button()
+        self.read_country()
+
+    def add_search_field(self):
+        """ Add QLineEdit """
+        self.search_field = QLineEdit(self.root)
+        self.search_field.move(200, 70)
+        self.search_field.resize(500, 50)
+        self.search_field.setStyleSheet(
+            "font-size:24px; background-color:white; padding: 0 15px; border:2px solid gray; border-radius:23px 23px;")
+        self.search_field.returnPressed.connect(self.check_connection)
+
+    def add_search_button(self):
+        self.search_button = QPushButton(self.root)
+        self.search_button.move(645, 75)
+        self.search_button.resize(40, 40)
+        self.search_button.setStyleSheet("background-color:white; border:0px;")
+        self.search_button.setIcon(QIcon("images//get_data.png"))
+        self.search_button.setIconSize(QSize(40, 40))
+        self.search_button.setToolTip("Click search to get weather")
+        self.search_button.clicked.connect(self.check_connection)
+
+    def read_country(self):
+        """ Read All Data """
+        with open('country.json', mode='r') as self.data:
+            self.data = json.loads(self.data.read())
+
+            for self.section in self.data['countries']:
+                for self.city_name in self.section['states']:
+                    self.list_country.append(self.city_name)
+
+        self.completer = QCompleter(self.list_country)
+        self.completer.setCaseSensitivity(0)
+        self.search_field.setCompleter(self.completer)
+
+    def check_connection(self):
+        """Verify the device is connected to the Internet"""
+        self.weather_api_url = 'https://openweathermap.org/'
+        self.timeout = 5
+        try:
+            request = requests.get(self.weather_api_url, timeout=self.timeout)
+            if request:
+                if self.search_field.text() == "":
+                    return QMessageBox.warning(self.root, "Error", "You can't leave this field empty!", QMessageBox.Yes)
+                self.get_data()
+
+        except (requests.ConnectionError, requests.Timeout) as exception:
+            self.message_connection = QMessageBox.warning(self.root, "Error", "Please check your network!",
+                                                          QMessageBox.Yes)
+
+    def get_data(self):
+        """The Function Get Data And , Verify the device is connected to the Internet"""
+        self.weather_api_data = requests.get(self.URL.format(self.search_field.text(), self.API))
+
+        if self.weather_api_data:
+            self.jsonData = self.weather_api_data.json()
+            self.city = self.jsonData['name']
+            self.country = self.jsonData['sys']['country']
+            self.temp_klv = self.jsonData['main']['temp']
+            self.temp_celcuis = (self.temp_klv - 273.15)
+            self.temp_fehr = (self.temp_klv - 273.15) * 9 / 5 + 32
+            self.weather = self.jsonData['weather'][0]['main']
+            self.pressure = self.jsonData['main']['pressure']
+            self.description = self.jsonData['weather'][0]['description']
+            self.humidity = self.jsonData['main']['humidity']
+            self.wind = self.jsonData['wind']['speed']
+
+            self.final_result = (
+                self.city, self.country, self.temp_klv, self.temp_celcuis,
+                self.temp_fehr, self.weather, self.pressure, self.wind, self.humidity,
+                self.description
+            )
+
+            if self.final_result:
+                self.show_data()
+
+    def show_data(self):
+        if self.is_guide_shown:
+            self.class_style.label_guide.deleteLater()
+            self.is_guide_shown = False
+
+        self.class_style.location.setText(self.final_result[0] + '-' + self.final_result[1])
+        self.class_style.temperature.setText(('{:.0f}°C , {:.0f}°F'.format(self.final_result[3], self.final_result[4])))
+        self.class_style.pressure.setText(str(self.final_result[6]))
+        self.class_style.humidity.setText(str(self.final_result[7]) + '%')
+        self.class_style.wind.setText(str(self.final_result[8]))
+
+        self.class_style.label_weather_now.setText('{:.0f}°C'.format(self.final_result[3]))
+        self.class_style.label_weather_now.setStyleSheet("background-color:#333; color:cyan; font-size:22px")
+        self.class_style.label_weather_now.move(580, 340)
+        self.class_style.label_weather_now.resize(55, 22)
+        self.class_style.description.setText(str(self.final_result[9]))
+
+        self.thread_get_icon = threading.Thread(target=self.get_icon, args=()).start()
+
+    def get_icon(self):
+        """Get Icon Current Weather!"""
+        self.data = requests.get(self.URL.format(self.search_field.text(), self.API))
+        self.file_josn = self.data.json()
+        self.icon = self.file_josn['weather'][0]['icon']
+        urllib.request.urlretrieve(f'http://openweathermap.org/img/wn/{self.icon}.png', 'images\\image_weather.png')
+
+        self.pixmap = QPixmap("images\\image_weather.png").scaled(160, 160)
+        self.class_style.label_icon.setPixmap(self.pixmap)
+        self.class_style.label_icon.move(450, 340)
+        self.class_style.label_icon.resize(220, 120)
+
+
+if __name__ == '__main__':
+    application = QApplication(sys.argv)
+    window = QWidget()
+    class_style = Main(window)
+    window.show()
+    sys.exit(application.exec_())
---
+++ style.py
@@ -0,0 +1,101 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+class StyleWindow:
+    def __init__(self, root):
+        self.root = root
+        self.root.setWindowTitle('Weather App')
+        self.root.setStyleSheet('background-color:#333;')
+        self.root.setGeometry(500, 200, 900, 700)
+        self.root.setWindowIcon(QIcon('images\\icon_app.ico'))
+        self.root.setFixedWidth(900)
+        self.root.setFixedHeight(700)
+        self.add_logo()
+        self.add_box()
+        self.add_titles()
+
+    def add_logo(self):
+        self.label_image_logo = QLabel(self.root)
+        self.pixmap_logo = QPixmap("images\\logo.png")
+        self.label_image_logo.setPixmap(self.pixmap_logo)
+        self.label_image_logo.move(240, 250)
+
+    def add_box(self):
+        self.label_titles = QLabel("", self.root)
+        self.label_titles.setStyleSheet("font-size:18px; background-color:#1ab5ef; color:white; border-radius: 14px;")
+        self.label_titles.move(30, 570)
+        self.label_titles.resize(840, 80)
+
+    def add_titles(self):
+        title_style = "font-size:19px; background-color:#1ab5ef; color:white; font-weight: bold;"
+        value_style = "font-size:16px; background-color:#1ab5ef; color:black; font-weight: bold;"
+        value_empty = "......."
+
+        self.label_location = QLabel("Location", self.root)
+        self.label_location.move(60, 578)
+        self.label_location.setStyleSheet(title_style)
+
+        self.label_temperature = QLabel("Temperature", self.root)
+        self.label_temperature.setStyleSheet(title_style)
+        self.label_temperature.move(190, 578)
+
+        self.label_description = QLabel("Description", self.root)
+        self.label_description.setStyleSheet(title_style)
+        self.label_description.move(360, 578)
+
+        self.label_wind = QLabel("Wind", self.root)
+        self.label_wind.setStyleSheet(title_style)
+        self.label_wind.move(520, 578)
+
+        self.label_humidity = QLabel("Humidity", self.root)
+        self.label_humidity.move(625, 578)
+        self.label_humidity.setStyleSheet(title_style)
+
+        self.label_pressure = QLabel("Pressure", self.root)
+        self.label_pressure.move(760, 578)
+        self.label_pressure.setStyleSheet(title_style)
+
+        self.location = QLabel(value_empty, self.root)
+        self.location.setStyleSheet(value_style)
+        self.location.move(60, 610)
+        self.location.resize(130, 28)
+
+        self.temperature = QLabel(value_empty, self.root)
+        self.temperature.setStyleSheet(value_style)
+        self.temperature.move(190, 610)
+        self.temperature.resize(140, 28)
+
+        self.description = QLabel(value_empty, self.root)
+        self.description.setStyleSheet(value_style)
+        self.description.move(360, 610)
+        self.description.resize(140, 28)
+
+        self.humidity = QLabel(value_empty, self.root)
+        self.humidity.setStyleSheet(value_style)
+        self.humidity.move(625, 610)
+        self.humidity.resize(125, 28)
+
+        self.wind = QLabel(value_empty, self.root)
+        self.wind.setStyleSheet(value_style)
+        self.wind.move(520, 610)
+        self.wind.resize(100, 28)
+
+        self.pressure = QLabel(value_empty, self.root)
+        self.pressure.setStyleSheet(value_style)
+        self.pressure.move(760, 610)
+        self.pressure.resize(110, 28)
+
+        self.label_icon = QLabel(self.root)
+        self.label_weather_now = QLabel(self.root)
+        self.label_weather = QLabel(self.root)
+
+        self.label_weather = QLabel("TODAY'S WEATHER", self.root)
+        self.label_weather.move(460, 270)
+        self.label_weather.resize(220, 44)
+        self.label_weather.setStyleSheet("background-color:#333; color:white; font-size:22px")
+
+        self.label_guide = QLabel("Enter country or city..", self.root)
+        self.label_guide.move(460, 320)
+        self.label_guide.resize(220, 44)
+        self.label_guide.setStyleSheet("background-color:#333; color:gray; font-size:18px")
